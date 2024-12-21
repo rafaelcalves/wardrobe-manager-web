5b610194a40d4ac74f34c46a3b3724343f06583b
Rafael Corrêa Alves <rafael_c_alves@hotmail.com>
2024-12-21 11:24:29 -0300

    build: added chackra ui to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Box,
+  Button,
+  Checkbox,
+  ClientOnly,
+  HStack,
+  Heading,
+  Progress,
+  RadioGroup,
+  Skeleton,
+  VStack,
+} from "@chakra-ui/react"
+import { ColorModeButton } from "./components/ui/color-mode"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function Page() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box textAlign="center" fontSize="xl" pt="30vh">
+      <VStack gap="8">
+        <img alt="chakra logo" src="/static/logo.svg" width="80" height="80" />
+        <Heading size="2xl" letterSpacing="tight">
+          Welcome to Chakra UI v3 + Vite
+        </Heading>
+
+        <HStack gap="10">
+          <Checkbox.Root defaultChecked>
+            <Checkbox.HiddenInput />
+            <Checkbox.Control>
+              <Checkbox.Indicator />
+            </Checkbox.Control>
+            <Checkbox.Label>Checkbox</Checkbox.Label>
+          </Checkbox.Root>
+
+          <RadioGroup.Root display="inline-flex" defaultValue="1">
+            <RadioGroup.Item value="1" mr="2">
+              <RadioGroup.ItemHiddenInput />
+              <RadioGroup.ItemControl>
+                <RadioGroup.ItemIndicator />
+              </RadioGroup.ItemControl>
+              <RadioGroup.ItemText lineHeight="1">Radio</RadioGroup.ItemText>
+            </RadioGroup.Item>
+
+            <RadioGroup.Item value="2">
+              <RadioGroup.ItemHiddenInput />
+              <RadioGroup.ItemControl>
+                <RadioGroup.ItemIndicator />
+              </RadioGroup.ItemControl>
+              <RadioGroup.ItemText lineHeight="1">Radio</RadioGroup.ItemText>
+            </RadioGroup.Item>
+          </RadioGroup.Root>
+        </HStack>
+
+        <Progress.Root width="300px" value={65} striped>
+          <Progress.Track>
+            <Progress.Range />
+          </Progress.Track>
+        </Progress.Root>
+
+        <HStack>
+          <Button>Let's go!</Button>
+          <Button variant="outline">bun install @chakra-ui/react</Button>
+        </HStack>
+      </VStack>
+
+      <Box pos="absolute" top="4" right="4">
+        <ClientOnly fallback={<Skeleton w="10" h="10" rounded="md" />}>
+          <ColorModeButton />
+        </ClientOnly>
+      </Box>
+    </Box>
   )
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { ThemeProvider } from "next-themes"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChakraProvider value={defaultSystem}>
+      <ThemeProvider attribute="class" disableTransitionOnChange>
+        <App />
+      </ThemeProvider>
+    </ChakraProvider>
+  </React.StrictMode>,
 )
